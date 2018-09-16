2c128f55e6d055b0179aaeddbe983cd863e1b50e
Kevin Martins <kevinmmartins@gmail.com>
2018-09-16 18:30:48 -0300

    Created basic project

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  users$: Object;
+  title= 'Users list';
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(
+      data => this.users$ = data
+    );
+  }
+
+}
